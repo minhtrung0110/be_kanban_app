58dd2980897c04297d8a114584c5a1297b2bf17c
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2023-07-11 15:55:47 +0700

    feat(App): config Connection MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URL)],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URL), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
